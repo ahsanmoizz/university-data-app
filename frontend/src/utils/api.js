d1c5761174314bc90b69648e972c36794c03efb5
Ahsan Moizz <salmaashrafhig@gmail.com>
2025-10-07 23:43:48 +0500

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API = axios.create({ baseURL: "http://localhost:5000/api" });
+const API = axios.create({ baseURL: "http://95.217.129.142:5000/api" });
 
 // attach token automatically
 API.interceptors.request.use((req) => {
